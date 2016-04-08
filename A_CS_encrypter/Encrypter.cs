1d880df24deefee9a41946aa217aae1b3de55957
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-08 16:03:51 +0200

    Class structure ready.

---
+++ A_CS_encrypter/Encrypter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//Encrypts a given text by changing each character to the following letter in the alphabet. Decrypts too.
+namespace A_CS_encrypter {
+
+    internal class Encrypter {
+
+        static string Encrypt(string textToEncrypt) {
+            var encryptedText = "";
+
+            return encryptedText; }
+
+        static string Decrypt(string textToDecrypt) {
+            var decryptedText = "";
+
+            return decryptedText; }
+
+        static void Main(string[] args) {
+            Console.WriteLine(Encrypt("Hello"));
+            Console.WriteLine(Decrypt("Ifjjp")); }
+    }
+}
