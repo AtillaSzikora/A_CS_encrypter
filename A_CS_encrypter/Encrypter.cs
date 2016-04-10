6d846431a38fc6c1dba33b3e8bdfab235dd1be65
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-10 13:55:11 +0200

    Some error handling added for non alphabetic chars.

--- A_CS_encrypter/Encrypter.cs
+++ A_CS_encrypter/Encrypter.cs
@@ -1,22 +1,28 @@
-﻿using System;
+﻿//Encrypts a given text by changing each character to the following letter in the alphabet. Can also decrypt.
 
-//Encrypts a given text by changing each character to the following letter in the alphabet. Decrypts too.
-namespace A_CS_encrypter {
+using System;
 
-    internal class Encrypter {
+internal class Encrypter {
+    private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private const string TextPattern = "N"; //N is 13th char from the 25 long alphabet (...counted from 0)
 
-        static string Encrypt(string textToEncrypt) {
-            var encryptedText = "";
+	private static string Encrypt(string textToEncrypt) {
+        var encryptedText = "";
+		foreach (var c in textToEncrypt) {
+			if (Alphabet.IndexOf(char.ToUpper(c)) != -1) {
+				encryptedText += Alphabet[Alphabet.IndexOf(char.ToUpper(c))*2 - 26]; }
+			else { encryptedText += c; } }
+		return encryptedText; }
 
-            return encryptedText; }
+	private static string Decrypt(string textToDecrypt) {
+        var decryptedText = "";
+		foreach (var c in textToDecrypt) {
+			if (Alphabet.IndexOf(char.ToUpper(c)) != -1) {
+				decryptedText += Alphabet[Alphabet.IndexOf(char.ToUpper(c)) / 2]; }
+			else { decryptedText += c; } }
+		return decryptedText; }
 
-        static string Decrypt(string textToDecrypt) {
-            var decryptedText = "";
-
-            return decryptedText; }
-
-        static void Main(string[] args) {
-            Console.WriteLine(Encrypt("Hello"));
-            Console.WriteLine(Decrypt("Ifjjp")); }
-    }
+	private static void Main(string[] args) {
+        Console.WriteLine("Text to encrypt: " + TextPattern + "\tEncrypted text: " + Encrypt(TextPattern));
+        Console.WriteLine("Text to decrypt: " + Encrypt(TextPattern) + "\tDecrypted text: " + Decrypt(Encrypt(TextPattern))); }
 }
